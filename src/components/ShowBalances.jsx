1c6a00d71dad4fc10eb7f87e785315aff96255d7
Doan Dat <144463212+CptDat9@users.noreply.github.com>
2024-12-11 23:15:11 +0700

    Update ShowBalances.jsx

--- src/components/ShowBalances.jsx
+++ src/components/ShowBalances.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Contract, formatUnits } from "ethers";
+import { Contract } from "ethers";
 import ERC20 from "../abis/ERC20.json";
 
-const ETHAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; 
-const USDTAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"; 
+const USDTAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 const ShowBalances = ({ address, provider }) => {
   const [balances, setBalances] = useState({ ETH: "0", USDT: "0" });
@@ -16,14 +15,12 @@ const ShowBalances = ({ address, provider }) => {
 
           const balanceETH = await provider.getBalance(address);
 
-          
           const USDT = new Contract(USDTAddress, ERC20, signer);
           const balanceUSDT = await USDT.balanceOf(address);
 
-          //  state
           setBalances({
-            ETH: formatUnits(balanceETH, 18), 
-            USDT: formatUnits(balanceUSDT, 18), 
+            ETH: balanceETH.toString(), 
+            USDT: balanceUSDT.toString(), 
           });
         } catch (error) {
           console.error("Error fetching balances:", error);
@@ -37,8 +34,8 @@ const ShowBalances = ({ address, provider }) => {
   return (
     <div style={{ textAlign: "center", margin: "20px" }}>
       <h3>Your Balances:</h3>
-      <p>ETH: {balances.ETH}</p>
-      <p>USDT: {balances.USDT}</p>
+      <p>ETH (Wei): {balances.ETH}</p>
+      <p>USDT (Token): {balances.USDT}</p>
     </div>
   );
 };
