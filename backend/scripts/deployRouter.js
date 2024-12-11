bdaa5e4ac51d529a518ba32d657428e3d3e592c1
Doan Dat <144463212+CptDat9@users.noreply.github.com>
2024-12-11 11:47:48 +0700

    Update deployRouter.js

--- backend/scripts/deployRouter.js
+++ backend/scripts/deployRouter.js
@@ -4,15 +4,12 @@ async function main() {
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contracts with the account:", deployer.address);
 
-    const factoryAddress = "0x9A676e781A523b5d0C0e43731313A708CB607508"; // Địa chỉ Factory
-    const ETHAddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"; // Địa chỉ ETH hoặc WETH token
-
-    // Lấy Contract Factory
+    const factoryAddress = "0x9A676e781A523b5d0C0e43731313A708CB607508"; 
+    const ETHAddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"; 
     const Router = await ethers.getContractFactory("UniswapV2Router02");
 
-    // Deploy Router contract
     const router = await Router.deploy(factoryAddress, ETHAddress);
-    await router.deploymentTransaction().wait(); // Chờ giao dịch hoàn tất
+    await router.deploymentTransaction().wait(); 
 
     console.log("UniswapV2Router02 deployed to:", await router.getAddress());
 }
