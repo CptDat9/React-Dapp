e197902577f68ac706a35fffeea79b8abae2036d
Doan Dat <144463212+CptDat9@users.noreply.github.com>
2024-12-11 11:21:05 +0700

    Update Liquidity.jsx

--- src/components/Liquidity.jsx
+++ src/components/Liquidity.jsx
@@ -5,7 +5,6 @@ import UniswapV2Pair from '../abis/UniswapV2Pair.json';
 
 const factoryAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 
-// Địa chỉ của ETH và USDT token mà bạn đã triển khai
 const ETH_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 const USDT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
