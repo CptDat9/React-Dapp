e10fb3d7a9431151554502ec822b98366a86dcbc
Doan Dat <144463212+CptDat9@users.noreply.github.com>
2024-12-11 11:48:10 +0700

    Update deploytoken.js

--- backend/scripts/deploytoken.js
+++ backend/scripts/deploytoken.js
@@ -1,19 +1,19 @@
-const { ethers } = require("hardhat"); // Import ethers từ Hardhat
+const { ethers } = require("hardhat"); 
 
 async function main() {
     const [deployer] = await ethers.getSigners();
     console.log("Deploy contract with account: " + deployer.address);
 
-    // Deploy ETH token
+
     const ETH = await ethers.getContractFactory("ETH");
     const eth = await ETH.deploy(ethers.parseEther("1000000"));
-    await eth.waitForDeployment(); // Chờ đợi giao dịch triển khai hoàn tất
+    await eth.waitForDeployment(); 
     console.log("ETH token deployed to:", await eth.getAddress());
 
-    // Deploy USDT token
+   
     const USDT = await ethers.getContractFactory("USDT");
     const usdt = await USDT.deploy("1000000000000");
-    await usdt.waitForDeployment(); // Chờ đợi giao dịch triển khai hoàn tất
+    await usdt.waitForDeployment(); 
     console.log("USDT token deployed to:", await usdt.getAddress());
 }
 
